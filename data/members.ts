dd6603be33124498047aa296348635a2bb7badfd
Keisuke Nakano <128650458+Honoka-Nakano@users.noreply.github.com>
2024-10-17 23:12:39 +0900

    modified data/members.ts & added default icon

--- data/members.ts
+++ data/members.ts
@@ -1,7 +1,7 @@
 export const members = [
   {
     name: "矢内勇生",
-    nameRomaji: "yuki yanai",
+    nameRomaji: "Yuki Yanai",
     position: "准教授",
     image: "yukiyanai.jpeg",
     description: "計量政治学の研究と教育",
@@ -20,7 +20,7 @@ export const members = [
 
   {
     name: "中野佳介",
-    nameRomaji: "keisuke nakano",
+    nameRomaji: "Keisuke Nakano",
     position: "学部生",
     image: "keisukenakano.png",
     description: "ジャム理論に基づいた研究テーマを掲げ，実験データを用いたベイズ統計分析を行う．",
